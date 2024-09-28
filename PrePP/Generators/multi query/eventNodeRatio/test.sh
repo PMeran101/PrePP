4c963a2ff35c3de728809adde5da13afbe3e12c3
PMeran101 <p.meran@campus.tu-berlin.de>
2024-09-28 15:36:51 +0200

    small change

---
+++ PrePP/Generators/multi query/eventNodeRatio/test.sh
@@ -0,0 +1,7 @@
+python generate_network.py 20 0.1
+python write_config_single.py
+python determine_all_single_selectivities.py
+python generate_projections.py
+python combigen.py
+python computePlanCosts.py eventNodeRatio
+python generateEvaluationPlan.py
